bf8996a1470244bf034c9a87fad44fe14ab25965
Andre Marcio <andre.marcio.ti@gmail.com>
2026-03-27 00:10:56 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ KNIGHTS: dict[
     dict[str, str | int | list[Any] | dict[str, str | int] | None]
 
     | dict[str, str | int | list[dict[str, str | int]]
-    | dict[str, str | int] | None]
+                    | dict[str, str | int] | None]
     | dict[str, str | int | list[dict[str, str | int]]]
     | dict[str, str | int]
     | dict[str, str | dict[str, int]]
